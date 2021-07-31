331b98e3838bf5e5068e1847ac1c7eb1b3743d74
Aaron T <a.thomson22@outlook.com>
2021-07-31 17:14:40 +0800

    Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "GameEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"NG_PLATFORM_WINDOWS",
+			"NG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "NG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NG_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "NG_DIST"
+		symbols "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Engine/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"NG_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "NG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NG_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "NG_DIST"
+		symbols "On"
